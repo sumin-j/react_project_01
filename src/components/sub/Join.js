c6746bc812227e35183a582ad7daadddcf20e64b
sumin-j <soommmni@gmail.com>
2022-05-13 01:27:06 +0900

    Add pages

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,10 +1,198 @@
 import Layout from '../common/Layout';
+import {useState, useEffect} from 'react';
 
 function Join() {
+  const initVal = {
+    id : '',
+    pw1 : '',
+    pw2 : '',
+    email : '',
+    language : '',
+    path : '',
+    comments :  ''
+  }
+
+  const [val, setVal] = useState(initVal);
+  const [err, setErr] = useState({});
+  const [success, setSuccess] = useState(false);
+  const [isSubmit,setIsSubmit] = useState(false);
+
+  const check = (val) => {
+    const errs = {};
+    const eng = /[a-zA-Z]/;
+    const num = /[0-9]/;
+    const spc = /[~!@#$%^&*()_+]/;
+
+    if(val.id.length < 5){
+      errs.id = 'Enter an ID of 5 or more characters';
+    }
+
+    if(val.pw1.length <5 || !eng.test(val.pw1) || !num.test(val.pw1) || !spc.text(val.pw1)) {
+      errs.pw1 = 'Password must contain at least 5 letters, numbers, and special characters.';
+    }
+
+    if(val.pw1 !== val.pw2 || !val.pw2) {
+      errs.pw2 = 'Please enter the same two passwords';
+    }
+
+    if(val.email.length < 8 || !/@/.test(val.email)) {
+      errs.email = 'Please enter your email at least 8 characters including @';
+    }
+    if(val.language ==='') {
+      errs.language = 'Choose your language';
+    }
+    if (!val.path) {
+      errs.path = 'Please select one or more';
+    }
+
+    if(val.comments.length < 10) {
+      errs.comments = 'Please enter 10 or more characters for comments.';
+    }
+    
+    return errs;
+  }
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setVal({...val, [name]:value})
+  }
+
+  const handleCheck = (e) => {
+    let isCheck = false;
+    const {name} = e.target;
+    const inputs = e.target.parentElement.querySelectorAll('input');
+
+    inputs.forEach(el => {
+      if(el.checked) isCheck = true;
+    });
+
+    setVal({...val,[name]:isCheck});
+  }
+
+  const handleSelect = (e) =>{
+    const {name} = e.target;
+    const isSelected = e.target.options[e.target.selectedIndex].value;
+    console.log(isSelected);
+    setVal({...val,[name]:isSelected});
+  }
+
+  const handleReset = () => {
+		setVal(initVal);
+		setErr({});
+	};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErr(check(val));
+  };
+
   return (
-   <Layout name={'Join'}>
+   <>
+    <Layout name={'Join'}>
      <p>description</p>
-   </Layout>
+    </Layout>
+    
+    <form action="#" onSubmit={handleSubmit}>
+      <fieldset>
+        <legend className='hidden'>Join form</legend>
+        <table>
+          <caption className='hidden'>Join form</caption>
+          <tbody>
+            {/* ID */}
+            <tr>
+              <th scope='row'>
+                <label htmlFor="id">ID</label>
+              </th>
+              <td>
+                <input type="text" placeholder='name' id='id' name='id' value={val.id} onChange={handleChange} />
+                <span className='err'>{err.id}</span>
+              </td>
+            </tr>
+            {/* PW1 */}
+            <tr>
+              <th scope='row'>
+                <label htmlFor="pw1">PASSWORD</label>
+              </th>
+              <td>
+                <input type="password" name="pw1" id="pw1" placeholder='Password' value={val.pw1} onChange={handleChange}/>
+                <span className="err">{err.pw1}</span>
+              </td>
+            </tr>
+            {/* PW2 */}
+            <tr>
+              <th scope='row'>
+                <label htmlFor="pw2">Confirm Password</label>
+              </th>
+              <td>
+                <input type="password" name="pw2" id="pw2" placeholder='Re-enter password' value={val.pw2} onChange={handleChange} />
+                <span className="err">{err.pw2}</span>
+              </td>
+            </tr>
+            {/* EMAIL */}
+            <tr>
+              <th scope='row'>
+                <label htmlFor="email">E-mail</label>
+              </th>
+              <td>
+                <input type="email" name="email" id="email" placeholder='Email address' value={val.email} onChange={handleChange}/>
+                <span className="err">{err.email}</span>
+              </td>
+            </tr>
+            {/* Language */}
+            <tr>
+              <th>
+                <label htmlFor="language">Language</label>
+              </th>
+              <td>
+                <select name="lan" id="lan" onChange={handleSelect}>
+                  <option value="">Select language</option>
+                  <option value="korean">Korean</option>
+                  <option value="english">English</option>
+                  <option value="spanish">Spanish</option>
+                  <option value="chinese">Chinese</option>
+                  <option value="japanese">Japanese</option>
+                  <span className="err">{err.language}</span>
+                </select>
+              </td>
+            </tr>
+            {/* Known path */}
+            <tr>
+              <th scope='row'>
+                <label htmlFor="path">Known path</label>
+              </th>
+              <td>
+                <label htmlFor="recommendation">Recommendation</label>
+                <input type="checkbox" name="path" id="recommendation" onChange={handleCheck} />
+                <label htmlFor="internet">Internet</label>
+                <input type="checkbox" name="pth" id="internet" onChange={handleCheck} />
+                <label htmlFor="leafleat">Leafleat</label>
+                <input type="checkbox" name="path" id="leafleat" onChange={handleCheck} />
+                <span className='err'>{err.path}</span>
+              </td>
+            </tr>
+            {/* Comments */}
+            <tr>
+              <th scope='row'>
+                <label htmlFor="comments">Comments</label>
+              </th>
+              <td>
+                <textarea name="comments" id="comments" cols="30" rows="10" value={val.comments} onChange={handleChange}></textarea>
+                <span className='err'>{err.comments}</span>
+              </td>
+            </tr>
+
+            <tr>
+              <th colSpan='2'>
+                <input type="reset" value="Cancle" onClick={handleReset} />
+                <input type="submit" value="Submit" onClick={()=>setIsSubmit(true)}/>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </fieldset>
+    </form>
+
+    </>
   )
 }
 
--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,10 +1,126 @@
 import Layout from '../common/Layout';
+import {useRef, useEffect, useState} from 'react';
+
+const path = process.env.PUBLIC_URL;
+
 
 function Location() {
+  const {kakao} = window;
+  const container = useRef(null);
+
+  const info = [
+    {
+      title : 'Shinsegae',
+      latlng : new kakao.maps.LatLng(37.5049693, 127.0042386),
+      imgSrc : `${path}/assets/marker.png`,
+      imgSize :new kakao.maps.Size(64, 69) ,
+      imgPos : {offset: new kakao.maps.Point(27, 69)}
+    },
+    {
+      title : 'Galleria',
+      latlng : new kakao.maps.LatLng(37.5281284, 127.0408146),
+      imgSrc : `${path}/assets/marker.png`,
+      imgSize :new kakao.maps.Size(64, 69) ,
+      imgPos : {offset: new kakao.maps.Point(27, 69)}
+    },
+    {
+      title : 'Hyundai',
+      latlng : new kakao.maps.LatLng(37.3925837, 127.1119894),
+      imgSrc : `${path}/assets/marker.png`,
+      imgSize :new kakao.maps.Size(64, 69) ,
+      imgPos : {offset: new kakao.maps.Point(27, 69)}
+    }
+  ]
+
+  const [map, setMap] = useState(null);
+  const branch = useRef(null);
+  const [mapInfo] = useState(info);
+  const [traffic, setTraffic] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  useEffect(()=>{
+    container.current.innerHTML = '';
+
+    const options = { 
+      center: mapInfo[index].latlng,
+      level: 3 
+  };
+    var map = new kakao.maps.Map(container.current, options);
+    
+    setMap(map);
+
+    const markerPosition = mapInfo[index].latlng;
+
+    const imageSrc = mapInfo[index].imgSrc; 
+    const imageSize = mapInfo[index].imgSize;
+    const imageOption = mapInfo[index].imgPos;
+
+    const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+    var marker = new kakao.maps.Marker({
+      position: markerPosition, 
+      image: markerImage // 마커이미지 설정 
+  });
+
+  marker.setMap(map); 
+
+      const branch_li = branch.current.querySelectorAll("li");
+      for(const btn of branch_li) btn.classList.remove("on");
+      branch_li[index].classList.add("on");
+
+      var mapTypeControl = new kakao.maps.MapTypeControl();
+      map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+      var zoomControl = new kakao.maps.ZoomControl();
+      map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+      const mapInit = () => { 
+        console.log('마커 중앙 유지');
+        map.setCenter(mapInfo[index].latlng);
+      }
+
+      window.addEventListener('resize',mapInit);
+
+      return() => {
+        window.removeEventListener('resize',mapInit)
+      }
+
+
+  },[index])
+
+  useEffect(()=>{
+    if (map) {
+      traffic
+        ? map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+        : map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+    }
+  },[traffic])
+
   return (
-    <Layout name={'Location'}>
-      <p>description</p>
-    </Layout>
+    <>
+      <Layout name={'Location'}>
+        <p>description</p>
+      </Layout>
+
+      <section className="map">
+        <div id="map" ref={container}></div>
+
+      <button onClick={()=>setTraffic(!traffic)}>
+        {traffic ? 'Traffic Off' : 'Traffic On'}
+      </button>
+     
+
+      <ul className='branch' ref={branch}>
+        {mapInfo.map((item,idx)=>{
+          return(
+            <li key={idx} onClick={()=>setIndex(idx)}>
+              {item.title}
+            </li>
+          )
+        })}
+      </ul>
+      </section>
+    </>
   )
 }
 
